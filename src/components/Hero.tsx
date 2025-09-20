fad40ed4af055f3e0b0498121ffe4c20b86542f1
Abdullah Riaz <onlyabdullahriaz@gmail.com>
2025-09-20 13:18:40 -0400

    Add scroll

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -308,7 +308,7 @@ Please help me find the best flight options for this route.`;
       </div>
 
       {/* Scroll Indicator */}
-      <div className="absolute bottom-20 left-1/2 transform -translate-x-1/2 animate-bounce">
+      <div className="absolute bottom-20 left-1/2 transform -translate-x-1/2 animate-bounce hidden md:block">
         <div className="w-6 h-10 border-2 border-white rounded-full flex justify-center">
           <div className="w-1 h-3 bg-white rounded-full mt-2 animate-pulse"></div>
         </div>
